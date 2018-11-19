00d9365aa380b0ae28abc354a87c233f6f814d98
Ben Don <benbdon@gmail.com>
2018-11-19 09:16:34 -0600

    Added pre-condition, switch, more operands. No considering order of ops.

--- basic_examples/calculator.cpp
+++ basic_examples/calculator.cpp
@@ -1,19 +1,35 @@
 #include "std_lib_facilities.h" 
 
-int main() { 
-    cout << "Please enter expression (we can handle + and –): "; 
+int main() {
+    cout << "Please enter expression (we can handle +, –, *, and /)\n";
+    cout << "add an x to end expression (e.g., 1 + 2 * 3x): ";
     int lval = 0; 
     int rval; 
-    char op; 
-    int res; 
-    cin >> lval >> op >> rval; // read something like 1 + 3
-
-    if (op =='+') 
-        res = lval + rval; // addition 
-    else if (op == '-') 
-        res = lval - rval; // subtraction 
-
-    cout << "Result: " << res << '\n'; 
-    keep_window_open(); 
-    return 0;
+    cin >> lval; // read leftmost operand 
+    if (!cin) error("no first operand"); 
+    for (char op; cin >> op;) { // read operator and right-hand operand 
+                                // repeatedly 
+        if (op!='x') cin >> rval; 
+        if (!cin) error("no second operand"); 
+        switch(op) {
+        case '+': 
+            lval += rval; // add: lval = lval + rval 
+            break; 
+        case '-':
+            lval -= rval; // subtract: lval = lval – rval 
+            break; 
+        case '*': 
+            lval *= rval; // multiply: lval = lval * rval 
+            break; 
+        case '/': 
+            lval /= rval; // divide: lval = lval / rval 
+            break; 
+        default: // not another operator: print result 
+            cout << "Result: " << lval << '\n'; 
+            keep_window_open(); 
+            return 0; 
+        }    
+    } 
+    error("bad expression"); 
 }
+
