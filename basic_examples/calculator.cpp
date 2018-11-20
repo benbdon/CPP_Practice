e4aa9ea48c295c67dd3fe3b367addc0cb1e48c3a
Ben Don <benbdon@gmail.com>
2018-11-20 08:22:01 -0600

    First try to use grammar results in infinite recursion

--- basic_examples/calculator.cpp
+++ basic_examples/calculator.cpp
@@ -1,35 +1,27 @@
 #include "std_lib_facilities.h" 
 
+class Token {     // a very simple user-defined type
+public:
+    char kind;
+    double value; 
+};
+
+
 int main() {
-    cout << "Please enter expression (we can handle +, –, *, and /)\n";
-    cout << "add an x to end expression (e.g., 1 + 2 * 3x): ";
-    int lval = 0; 
-    int rval; 
-    cin >> lval; // read leftmost operand 
-    if (!cin) error("no first operand"); 
-    for (char op; cin >> op;) { // read operator and right-hand operand 
-                                // repeatedly 
-        if (op!='x') cin >> rval; 
-        if (!cin) error("no second operand"); 
-        switch(op) {
-        case '+': 
-            lval += rval; // add: lval = lval + rval 
-            break; 
-        case '-':
-            lval -= rval; // subtract: lval = lval – rval 
-            break; 
-        case '*': 
-            lval *= rval; // multiply: lval = lval * rval 
-            break; 
-        case '/': 
-            lval /= rval; // divide: lval = lval / rval 
-            break; 
-        default: // not another operator: print result 
-            cout << "Result: " << lval << '\n'; 
-            keep_window_open(); 
-            return 0; 
-        }    
-    } 
-    error("bad expression"); 
+    Token t1 {'+'};
+    Token t2 {'8',11.5};
 }
 
+Token get_token(); // function to read a token from cin 
+vector<Token> tok; // we’ll put the tokens here 
+int main() {
+    while (cin) {
+        Token t = get_token();
+        tok.push_back(t);
+    } 
+    for (int i = 0; i < tok.size(); ++i) {
+        if (tok[i].kind == '*') { // we found a multiply! 
+            double d = tok[i - 1].value * tok[i + 1].value; 
+    // now what? 
+    } 
+}
