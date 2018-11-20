a996b6c7f4ed3f06ba2c82087b5bcc790971c725
Ben Don <benbdon@gmail.com>
2018-11-20 08:48:44 -0600

    One more attempt

--- basic_examples/calculator.cpp
+++ basic_examples/calculator.cpp
@@ -1,5 +1,5 @@
 #include "std_lib_facilities.h" 
-// try #2 doesn't work yet because 1-2-3 is parsed as 1-(2-3)
+// try #3 doesn't work yet and is redundant with tests for + and get_token()
 
 class Token {     // a very simple user-defined type
 public:
@@ -13,16 +13,14 @@ Token get_token() { // read characters and compose tokens
 double expression() { // deal with + and – 
     double left = term(); // read and evaluate an Expression
     Token t = get_token(); // get the next token
-    switch(t.kind) { // see which kind of token it is
-        case '+':
-            return left + expression(); //read and evaluate a Term,
-                                    // then do an add
-        case '-':
-            return left - expression(); // read and evaluate a Term,
-                                    // then do a subtraction
-        default:
-            return left;
-        }   
+    while(t.kind == '+' || t.kind == '-') { // look for a + or -
+        if(t.kind =='+')
+            left += term(); // evaluate Term and add
+        else
+            left -= term(); // evaluate Term and subtract
+        t = get_token();
+        }
+        return left;   // finally: no more + or -; return the answer
     }
 }
 double term(){ // deal with *, /, and % 
