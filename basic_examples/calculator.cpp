9a20274aa609ac5be6a4a671221da9c29981e9a0
Ben Don <benbdon@gmail.com>
2018-11-22 09:06:33 -0600

    added a % case to ts.get and format fix

--- basic_examples/calculator.cpp
+++ basic_examples/calculator.cpp
@@ -55,20 +55,18 @@ Token Token_stream::get()
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
     
     switch (ch) {
-    case '=':    // for "print"
-    case 'x':    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/': 
-        return Token(ch);        // let each character represent itself
-    case '.':
-    case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8': case '9':
-    {   cin.putback(ch);         // put digit back into the input stream
-        double val;
-        cin >> val;              // read a floating-point number
-        return Token('8',val);   // let '8' represent "a number"
-    }
-    default:
-        error("Bad token");
+        case '=':    // for "print"
+        case 'x':    // for "quit"
+        case '(': case ')': case '+': case '-': case '*': case '/': case '%':
+            return Token(ch);        // let each character represent itself
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9': {   cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;              // read a floating-point number
+            return Token('8',val);   // let '8' represent "a number"
+        } default:
+            error("Bad token");
     }
 }
 //---
