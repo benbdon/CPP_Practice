0466648c0c39e339d70576d327894222fbeb38da
Ben Don <benbdon@gmail.com>
2018-11-22 09:54:16 -0600

    simplify format and add/remove comments

--- basic_examples/calculator.cpp
+++ basic_examples/calculator.cpp
@@ -1,3 +1,44 @@
+/*
+    Simple calculator 
+
+    Revision history:
+        Revised by Ben Don November 2018
+        Revised by Bjarne Stroustrup November 2013 
+        Originally written by Bjarne Stroustrup 
+            (bs@ cs.tamu.edu) Spring 2004. 
+
+    This program implements a basic expression calculator. 
+    Input from cin; output to cout. 
+    The grammar for input is: 
+
+    Statement: 
+        Expression
+        Print 
+        Quit
+    Print:
+        =
+    Quit:
+        q
+    Expression:
+        Term
+        Expression + Term
+        Expression - Term
+    
+    Term:
+        Primary
+        Term * Primary
+        Term / Primary
+        Term % Primary
+    Primary:
+        Number
+        ( Expression )
+        - Primary
+        + Primary
+    Number:
+        floating-point-literal
+
+    Input comes from cin through the Token_stream called ts.
+*/
 #include "std_lib_facilities.h" 
 
 //---
@@ -54,26 +95,31 @@ void Token_stream::putback(Token t)
 
 Token Token_stream::get()
 {
-    if (full) {       // do we already have a Token ready?
-        full = false; // remove token from buffer
+    if (full) {       // check if we already have a Token ready
+        full = false; 
         return buffer;
     } 
-    
     char ch;
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
     
     switch (ch) {
-        case print:    // for "print"
-        case quit:    // for "quit"
-        case '(': case ')': case '+': case '-': case '*': case '/': case '%':
-            return Token(ch);        // let each character represent itself
-        case '.':
+        case quit:    
+        case print:    
+        case '(': 
+        case ')': 
+        case '+': 
+        case '-': 
+        case '*': 
+        case '/': 
+        case '%':
+            return Token(ch); // let each character represent itself
+        case '.': // a floating-point-literal can start with a dot
         case '0': case '1': case '2': case '3': case '4':
-        case '5': case '6': case '7': case '8': case '9': {   
+        case '5': case '6': case '7': case '8': case '9': { // numeric literal    
             cin.putback(ch);         // put digit back into the input stream
             double val;
             cin >> val;              // read a floating-point number
-            return Token(number,val);   // let '8' represent "a number"
+            return Token(number,val);
         } default:
             error("Bad token");
     }
@@ -177,6 +223,7 @@ void calculate() {
 }
 
 //---
+
 int main()
 try {
     cout << "Welcome to our simple calculator.\nPlease enter expressions using floating-point numbers.\n";
