6df2ed054aad4ce9a2abf3355a5879b83a622d39
Ben Don <benbdon@gmail.com>
2018-11-27 16:06:17 -0600

    developed some UDT for month and year

--- basic_examples/date.cpp
+++ basic_examples/date.cpp
@@ -1,20 +1,41 @@
 #include "std_lib_facilities.h"
-// simple Date (control access) 
+ 
+enum class Month { 
+    jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec 
+};
+
+class Year {
+    static constexpr int min = 1800;
+    static constexpr int max = 2200;
+public:
+    class Invalid { };
+    Year(int x)
+        : y{x} {
+            if (x<min || max<=x) throw Invalid{};
+        }
+        int year() {
+            return y;
+        }
+private:
+    int y;
+};
 
 class Date {  
 public:
     class Invalid { };
-    Date(int y, int m, int d); // check for valid date and initialize
+    Date(Year y, Month m, int d); // check for valid date and initialize
     void add_day( int n); // increase the Date by n days 
-    int month() { return m; } 
+    Month month() { return m; } 
     int day() { return d; } 
-    int year() { return y; } 
+    Year year() { return y; } 
 private:
-    int y, m, d; // year, month, day
+    Year y;
+    Month m;
+    int d;
     bool is_valid();
 };
 
-Date::Date(int yy, int mm, int dd) // constructor 
+Date::Date(Year yy, Month mm, int dd) // constructor 
     :y{yy}, m{mm}, d{dd} // (member) initializer list
 {
         if(!is_valid()) throw Invalid{}; // check for validity
@@ -22,21 +43,24 @@ Date::Date(int yy, int mm, int dd) // constructor
 void Date::add_day(int n) {
     // . . . 
 } 
-int Date::month() { 
-    return m; // not the member function, can’t access m
+Month Date::month() { 
+    return m;
 }
 
 bool Date::is_valid() { // return true if date is valid
-    if (m<1 || 12<m) return false; 
+    if (int(m)<1 || 12<int(m)) return false; 
     // . . . 
 }
 
 
 void main() 
 try {
-    Date dxy {2004, 13, 1};
-    cout << dxy << '\n'; // still need to explain what cout should do with out UDT
-    dxy.add_day(2);
+Date dx1 {Year{1998}, 4, 3}; // error: 2nd argument not a Month 
+Date dx2 {Year{ 1998}, 4, Month:: mar}; // error: 2nd argument not a Month 
+Date dx2 {4, Month:: mar, Year{ 1998}}; // error: 1st argument not a Year 
+Date dx2 {Month:: mar, 4, Year{ 1998}}; // error: 2nd argument not a Month 
+Date dx3 {Year{ 1998}, Month:: mar, 30}; // OK
+
 }
 catch (Date::Invalid) {
     error("invalid date"); // error() defined in § 5.6.3
