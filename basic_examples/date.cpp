d354f2d887cbdcfaa6c0c5d285db41a83c6b51b7
Ben Don <benbdon@gmail.com>
2018-11-27 09:30:11 -0600

    adding error checking to our UDT for date

--- basic_examples/date.cpp
+++ basic_examples/date.cpp
@@ -1,17 +1,43 @@
 #include "std_lib_facilities.h"
 // simple Date (control access) 
 
-class Date {
-    int y, m, d; // year, month, day 
-public: 
+class Date {  
+public:
+    class Invalid { };
     Date(int y, int m, int d); // check for valid date and initialize
     void add_day( int n); // increase the Date by n days 
     int month() { return m; } 
     int day() { return d; } 
     int year() { return y; } 
+private:
+    int y, m, d; // year, month, day
+    bool is_valid();
 };
 
-void main() {
-    Date birthday {1970, 12, 30}; // OK 
-    cout << birthday.month() << '\n'; // we provided a way to read m
+Date::Date(int yy, int mm, int dd) // constructor 
+    :y{yy}, m{mm}, d{dd} // (member) initializer list
+{
+        if(!is_valid()) throw Invalid{}; // check for validity
+}
+void Date::add_day(int n) {
+    // . . . 
+} 
+int Date::month() { 
+    return m; // not the member function, can’t access m
+}
+
+bool Date::is_valid() { // return true if date is valid
+    if (m<1 || 12<m) return false; 
+    // . . . 
+}
+
+
+void main() 
+try {
+    Date dxy {2004, 13, 1};
+    cout << dxy << '\n'; // still need to explain what cout should do with out UDT
+    dxy.add_day(2);
+}
+catch (Date::Invalid) {
+    error("invalid date"); // error() defined in § 5.6.3
 }
