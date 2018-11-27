ed9153177231d6deefab9d784a5e077ee9968bd5
Ben Don <benbdon@gmail.com>
2018-11-26 18:52:59 -0600

    first cut of a class to represent the date

---
+++ basic_examples/date.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+// simple Date (control access) 
+
+class Date {
+    int y, m, d; // year, month, day 
+public: 
+    Date(int y, int m, int d); // check for valid date and initialize
+    void add_day( int n); // increase the Date by n days 
+    int month() { return m; } 
+    int day() { return d; } 
+    int year() { return y; } 
+};
+
+void main() {
+    Date birthday {1970, 12, 30}; // OK 
+    cout << birthday.month() << '\n'; // we provided a way to read m
+}
