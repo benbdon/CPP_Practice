f8111ab1991bb6102a841dab8d292fb0833f2f02
Ben Don <benbdon@gmail.com>
2018-12-03 17:37:54 -0600

    working code - had a != where it should have been a ==

--- Part_II/structured_data.cpp
+++ Part_II/structured_data.cpp
@@ -2,31 +2,6 @@
 
 const int not_a_reading = -7777; // less than absolute zero
 const int not_a_month = -1;
-constexpr int implausible_min = -200; 
-constexpr int implausible_max = 200; 
-
-vector<string> month_input_tbl = { 
-    "jan", "feb", "mar", "apr", "may", "jun", "jul", 
-    "aug", "sep", "oct", "nov", "dec" 
-}; 
-
-vector<string> month_print_tbl = { 
-    "January", "February", "March", "April", "May", "June", "July", 
-    "August", "September", "October", "November", "December" }; 
-
-int month_to_int(string s) {
-    // is s the name of a month? If so return its index [0: 11] otherwise –1  
-    for(int i=0; i<12; ++i) 
-        if (month_input_tbl[i]==s) 
-            return i; 
-    return -1; 
-} // in the future, use a map, but for now this method is better than a ton of if/then
-
-string int_to_month( int i) { // months [0: 11]  
-    if (i<0 || 12<=i) error("bad month index"); 
-    return month_print_tbl[i];
-}
-
 
 struct Day {
     vector<double> hour{vector<double>(24,not_a_reading)};
@@ -48,30 +23,12 @@ struct Reading {
     double temperature; 
 }; 
 
-void end_of_loop(istream& ist,char term,const string& message) {
-    if(ist.fail()) {
-        ist.clear();
-        char ch;
-        if(ist>>ch && ch==term) return; // all is fine
-        error(message);
-    }
-}
-
-bool is_valid(const Reading& r) { 
-    // a rough test  
-    if (r.day<1 || 31<r.day) return false; 
-    if (r.hour<0 || 23<r.hour) return false; 
-    if (r.temperature<implausible_min || implausible_max<r.temperature) 
-        return false; 
-    return true;
-}
-
 istream& operator>>(istream& is, Reading& r) {
     // read a temperature reading from is into r 
     // format: ( 3 4 9.7 ) 
     // check format, but don’t bother with data validity
     char ch1; 
-    if (is>>ch1 && ch1=='(') { // could it be a Reading? 
+    if (is>>ch1 && ch1!='(') { // could it be a Reading? 
         is.unget(); 
         is.clear(ios_base::failbit); 
         return is; 
@@ -87,6 +44,52 @@ istream& operator>>(istream& is, Reading& r) {
     r.hour = h; 
     r.temperature = t; 
     return is; 
+}
+
+
+void end_of_loop(istream& ist,char term,const string& message) {
+    if(ist.fail()) {
+        ist.clear();
+        char ch;
+        if(ist>>ch && ch==term) return; // all is fine
+        error(message);
+    }
+}
+
+constexpr int implausible_min = -200; 
+constexpr int implausible_max = 200; 
+
+bool is_valid(const Reading& r) { 
+    // a rough test  
+    if (r.day<1 || 31<r.day) return false; 
+    if (r.hour<0 || 23<r.hour) return false; 
+    if (r.temperature<implausible_min || implausible_max<r.temperature) 
+        return false; 
+    return true;
+}
+
+
+vector<string> month_input_tbl = { 
+    "jan", "feb", "mar", "apr", "may", "jun", "jul", 
+    "aug", "sep", "oct", "nov", "dec" 
+}; 
+
+int month_to_int(string s) {
+    // is s the name of a month? If so return its index [0: 11] otherwise –1  
+    for(int i=0; i<12; ++i) 
+        if (month_input_tbl[i]==s) return i; 
+    //return -1; 
+} // in the future, use a map, but for now this method is better than a ton of if/then
+
+
+vector<string> month_print_tbl = { 
+    "January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December" }; 
+
+
+string int_to_month(int i) { // months [0: 11]  
+    if (i<0 || 12<=i) error("bad month index"); 
+    return month_print_tbl[i];
 }
 
 istream& operator>>(istream& is,Month& m) { 
@@ -110,8 +113,7 @@ istream& operator>>(istream& is,Month& m) {
         if (is_valid(r)) {
             if (m.day[r.day].hour[r.hour]!=not_a_reading) ++duplicates; 
             m.day[r.day].hour[r.hour] = r.temperature; 
-        } else 
-            ++invalids; 
+        } else ++invalids; 
     } 
     if (invalids) error("invalid readings in month", invalids);
     if (duplicates) error("duplicate readings in month", duplicates); 
@@ -146,10 +148,21 @@ istream& operator>>(istream& is,Year& y) {
     return is;
 }
 
-int main() {
+ostream& operator<<(ostream& os, Year& y) {
+    os << y.year;
+    return os;
+}
+ 
+void print_year(ostream& ost, Year& y) {
+    ost << y << endl;
+}
+
+int main() 
+try {
     cout<<"Please enter input file name\n"; 
     string iname; 
-    cin>>iname; 
+    cin>>iname;
+
     ifstream ifs {iname}; 
     if (!ifs) error("can't open input file", iname); 
 
@@ -165,11 +178,20 @@ int main() {
     // read an arbitrary number of years: 
     vector<Year> ys; 
     while(true) {
+        cout<<"OMG WE DID THIS MUCH"<<endl;
         Year y; // get a freshly initialized Year each time around 
         if(!(ifs>>y)) break; 
         ys.push_back(y); 
     }
     cout<<"read "<<ys.size()<<" years of readings\n";
 
-    //for (Year& y : ys) print_year(ofs, y);
+    for (Year& y : ys) print_year(ofs, y);
+}
+catch (runtime_error e) {
+    cout << e.what() << endl;
+    keep_window_open();
+}
+catch (...) {
+    cout << "Exiting" << endl;
+    keep_window_open();
 }
