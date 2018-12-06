1e8d1653bd39b810bedc2dece4b03bbb02ece79e
Ben Don <benbdon@gmail.com>
2018-12-05 22:37:10 -0600

    no obvious changes

---
+++ Part_II/integer_output.cpp
@@ -0,0 +1,8 @@
+#include "../Part_I/std_lib_facilities.h"
+
+int main() {
+    int myBirthday = 31;
+    cout<<myBirthday<<"\t(decimal)\n"
+    <<hex<<myBirthday<<"\t(hexadecimal\n"
+    <<oct<<myBirthday<<"\t(octal)\n";
+}
